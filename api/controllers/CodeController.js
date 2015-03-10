5663bb6b6dc0e8d82158ecf694401d001ddbe40a
TheFinestArtist <contact@thefinestartist.com>
2015-03-10 21:47:44 +0900

    Add azimuth

--- api/controllers/CodeController.js
+++ api/controllers/CodeController.js
@@ -16,7 +16,7 @@ module.exports = {
 
 		Sensor
 		.find({
-			azimuth: { '>': azimuth - 30, '<': azimuth + 30 },
+			azimuth: { '>': azimuth - 50, '<': azimuth + 50 },
 			pitch: { '>': pitch - 5, '<': pitch + 5 },
 			roll: { '>': roll - 5, '<': roll + 5 },
 			latitude: { '>': latitude - 0.001, '<': latitude + 0.001 },
